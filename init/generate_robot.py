8a7dee2c039a2c9cdf0cde56facef2550798b793
Alberto <albertoserranoleon@gmail.com>
2019-02-08 14:25:48 +0100

    Updated Generate_robot

--- init/generate_robot.py
+++ init/generate_robot.py
@@ -8,105 +8,13 @@ import os
 import sys
 import fileinput
 import urllib.request
+from github import Github
 
 __url__ = 'https://raw.githubusercontent.com/Pyro4Bot-RoboLab/Components/developing/'
 __json_bot_file__ = ""
 
 
-def get_sha_for_tag(repository, tag):
-    """
-    Returns a commit PyGithub object for the specified repository and tag.
-    """
-    branches = repository.get_branches()
-    matched_branches = [match for match in branches if match.name == tag]
-    if matched_branches:
-        return matched_branches[0].commit.sha
-
-    tags = repository.get_tags()
-    matched_tags = [match for match in tags if match.name == tag]
-    if not matched_tags:
-        raise ValueError('No Tag or Branch exists with that name')
-    return matched_tags[0].commit.sha
-
-
-def download_directory(repository, sha, server_path):
-    """
-    Download all contents at server_path with commit tag sha in
-    the repository.
-    """
-    contents = repository.get_dir_contents(server_path, ref=sha)
-
-    for content in contents:
-        print("Processing ", content.path)
-        if content.type == 'dir':
-            download_directory(repository, sha, content.path)
-        else:
-            try:
-                path = content.path
-                file_content = repository.get_contents(path, ref=sha)
-                file_data = base64.b64decode(file_content.content)
-                file_out = open(content.name, "w")
-                file_out.write(file_data)
-                file_out.close()
-            except (GithubException, IOError) as exc:
-                logging.error('Error processing %s: %s', content.path, exc)
-
-
-def update_robot(bot_name):
-    """ It checks and updates the directories and files needed to run the robot.
-    this only can be used once the user has described its robot in the json file.
-
-    If the services and components of the robots are already in the repository, they will be downloaded.
-    If not, the user must have developed the necessary files to handle those dependencies of the robots.
-    If neither of them are completed, this will show an error message to the user.
-    """
-    sys.path.append('../developing')
-    from node.libs.myjson import MyJson
-    from github import Github
-
-    github = Github(login_or_token="17143d9e9f8b00012627e2545eefce8fda07d216")
-    print(github.get_project('pyro4bot'))
-    #   github = Github(base_url='https://github.com/BertoSerrano/pyro4bot_components',
-    #                   login_or_token='17143d9e9f8b00012627e2545eefce8fda07d216')
-
-    variable = github.get_user()
-    print(variable)
-    variable = variable.get_repos()
-    print(variable)
-    for repo in github.get_user().get_repos():
-        print(repo.name)
-
-    # org = github.get_organization('BertoSerrano')
-    # repo = github.get_repos("BertoSerrano/pyro4bot_components")
-    # contents = repo.get_page("README.md")
-    # print("using pygithub library! \n")
-    # print(contents)
-
-    # repository_name = 'pyro4bot_components'
-    # repository = organization.get_repo(repository_name)
-    # sha = get_sha_for_tag(repository, )
-
-    _path_ = os.path.join('robots', bot_name)
-    __json_bot_file__ = os.path.join(_path_, bot_name + '.json')
-    json = MyJson(filename=__json_bot_file__).json
-
-    services_classes = []
-    components_classes = []
-
-    for (key, value) in json['services'].items():
-        class_name = value['cls']
-        services_classes.append(class_name)
-        file_url = __url__ + 'services/'  # TODO: not completed
-        file = _path_ + 'services/' + class_name + '.py'
-    #    urllib.request.urlretrieve(file_url, file)  # TODO: not completed
-
-    for (key, value) in json['components'].items():
-        components_classes.append(value['cls'])
-
-    print(services_classes)
-    print(components_classes)
-
-
+# TODO : make it more abstract => defaul template // specific element template
 def __create_template__(url_module, local_path):
     """ It creates the templates for the components that must be develop """
     file = os.path.join(local_path, 'Template.py')
@@ -127,14 +35,83 @@ def __create_json__(path, bot_name):
         raise
 
 
-def create_robot(bot_name):
+def __extract_element__():
+    pass
+
+
+def __find_element__():
+    pass
+
+
+def __download_directory__():
+    pass
+
+
+def __update_robot__(bot_name):
+    """ It checks and updates the directories and files needed to run the robot.
+    this only can be used once the user has described its robot in the json file.
+
+    If the services and components of the robots are already in the repository, they will be downloaded.
+    If not, the user must have developed the necessary files to handle those dependencies of the robots.
+    If neither of them are completed, this will show an error message to the user.
+    """
+    sys.path.append('../developing')
+    from node.libs.myjson import MyJson
+
+    #  github = Github(login_or_token="17143d9e9f8b00012627e2545eefce8fda07d216")
+    #    print(github.get_project(id=0))
+    #   github = Github(base_url='https://github.com/Pyro4Bot-RoboLab/Components',
+    #                   login_or_token='17143d9e9f8b00012627e2545eefce8fda07d216')
+
+    github = Github("pyro4bot", "90racano")
+
+    org = github.get_user().get_orgs()[0]
+    print(org)
+    repository = org.get_repo('Components')
+    print(repository)
+    stable = repository.get_contents(path='components/stable')
+    developing = repository.get_contents(path='components/developing')
+    for com in stable:
+        print(com)
+        print(com.path)
+        print(type(com))
+
+    # sha = get_sha_for_tag(repository, 'infrared')
+    # print(sha)
+
+    #   download_directory(repository=repository, server_path='stable/infrared')
+
+    _path_ = os.path.join('robots', bot_name)
+    __json_bot_file__ = os.path.join(_path_, bot_name + '.json')
+    json = MyJson(filename=__json_bot_file__).json
+
+    services_classes = []
+    components_classes = []
+
+    for (key, value) in json['services'].items():
+        class_name = value['cls']
+        services_classes.append(class_name)
+        #    if class_name in [com.path for com in stable]:
+        #       urllib.request.urlretrieve(com.path)
+        file_url = __url__ + 'services/'  # TODO: not completed
+        file = _path_ + 'services/' + class_name + '.py'
+    #    urllib.request.urlretrieve(file_url, file)  # TODO: not completed
+
+    print("services classes")
+    print(services_classes)
+
+    for (key, value) in json['components'].items():
+        components_classes.append(value['cls'])
+
+    print(services_classes)
+    print(components_classes)
+
+
+def __create_robot__(bot_name):
     """ The first execution of this program will create the structure, files and directories needed to a
     pyro4bot robot
     """
-    path = 'robots'
-    if not os.path.exists(path):
-        os.makedirs(path)
-    path = os.path.join(path, bot_name)
+    path = os.path.join('robots', bot_name)
     if not os.path.exists(path):
         os.makedirs(path)
     for module in ['services', 'components']:
@@ -165,7 +142,7 @@ if __name__ == "__main__":
             if argument == '-update':
                 argument = sys.argv[2]
                 argument.replace('.json', '')  # just having the precaution of not misspelling the name
-                update_robot(argument)
+                __update_robot__(argument)
         else:
             argument = sys.argv[1]
             if argument == '-update':
@@ -173,7 +150,7 @@ if __name__ == "__main__":
                 os._exit(0)
             else:
                 argument.replace('.json', '')  # just having the precaution of not misspelling the name
-                create_robot(argument)
+                __create_robot__(argument)
     except (KeyboardInterrupt, SystemExit):
         os._exit(0)
     except Exception:
