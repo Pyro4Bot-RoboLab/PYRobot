2ade028a9a34e2ac5e8d222aabd5ea344ec569e4
Alberto <albertoserranoleon@gmail.com>
2019-02-11 12:24:56 +0100

    Full generate_robot works with conditions
    There are still things to do: mostly: make it more abstract and refactoring some code.

--- init/generate_robot.py
+++ init/generate_robot.py
@@ -10,22 +10,24 @@ import fileinput
 import urllib.request
 from github import Github
 
-__url__ = 'https://raw.githubusercontent.com/Pyro4Bot-RoboLab/Components/developing/'
-__json_bot_file__ = ""
+sys.path.append('../developing')
+
+__base_url__ = 'https://raw.githubusercontent.com/Pyro4Bot-RoboLab/Components/developing/'
+__json_bot_filepath__ = ""
 
 
 # TODO : make it more abstract => defaul template // specific element template
 def __create_template__(url_module, local_path):
     """ It creates the templates for the components that must be develop """
     file = os.path.join(local_path, 'Template.py')
-    file_url = __url__ + url_module + '/Template.py'
+    file_url = __base_url__ + url_module + '/Template.py'
     urllib.request.urlretrieve(file_url, file)
 
 
 def __create_json__(path, bot_name):
     """ It creates the json file for the robot from a template """
     __json_bot_file__ = os.path.join(path, bot_name + '.json')
-    json_url = __url__ + 'init/bot_template.json'
+    json_url = __base_url__ + 'init/bot_template.json'
     urllib.request.urlretrieve(json_url, __json_bot_file__)
     try:
         for line in fileinput.input([__json_bot_file__], inplace=True):
@@ -35,19 +37,85 @@ def __create_json__(path, bot_name):
         raise
 
 
-def __extract_element__():
+def __extract_element__(bot_name):
     """ It inspects the bot's json file searching for the components and services of the robot """
-    pass
+    from node.libs.myjson import MyJson
+
+    _path_ = os.path.join('robots', bot_name)
+    __json_bot_file__ = os.path.join(_path_, bot_name + '.json')
+    json = MyJson(filename=__json_bot_file__).json
+
+    json_services_classes = []
+    json_components_classes = []
+
+    for (key, value) in json['services'].items():
+        json_services_classes.append(value['cls'])
+    for (key, value) in json['components'].items():
+        json_components_classes.append(value['cls'])
+
+    return json_services_classes, json_components_classes
 
 
-def __find_element__():
+# TODO : More efficient!!
+def __find_element__(module, json_module_classes, repository):
     """ It searches the element (component of service) in the repository of Pyro4Bot and return the url path """
-    pass
+    stable = repository.get_contents(path=module + '/stable')
+    developing = repository.get_contents(path=module + '/developing')
+    routes = []
+    for element in stable:
+        routes.append(element.path)
+    stable = routes
+    routes = []
+    for element in developing:
+        routes.append(element.path)
+    developing = routes
+
+    routes = []
+    flag = False
+    for element in json_module_classes:
+        for remote in stable:
+            if element in remote:
+                routes.append(remote)
+                flag = True
+        if not flag:
+            for remote in developing:
+                routes.append(remote)
+                flag = True
+        flag = False
+
+    return routes
 
 
-def __download_directory__():
+def __download_element__(bot_name, url_directory):
     """ It downloads the directory of the component or service from the repository of GitHub """
-    pass
+    local_path = os.path.join('robots', bot_name)
+    element_name = []
+    url_element = []
+    for each in url_directory:
+        aux = each.split('/')
+        aux = os.path.join(*aux)
+        file = os.path.join(local_path, aux)
+        if not os.path.exists(file):
+            os.makedirs(file)
+        for string in each.split('/'):
+            if string not in ('components', 'services', 'stable', 'developing'):
+                element_name.append(string)
+                url_element.append(each + '/' + string + '.py')
+
+    for element in url_element:
+        element_name = element.split('/')
+        element_name = os.path.join(*element_name)
+        file = os.path.join(local_path, element_name)
+        file_url = __base_url__ + element
+        urllib.request.urlretrieve(file_url, file)
+
+
+def __download_start__(bot_name):
+    # TODO: Download start_node.py as start.py
+    url = 'https://raw.githubusercontent.com/Pyro4Bot-RoboLab/Pyro4Bot/developing/developing/start_pyro4bot.py'
+    file = os.path.join('robots', bot_name)
+    file = os.path.join(file, 'start.py')
+    urllib.request.urlretrieve(url, file)
 
 
 def __update_robot__(bot_name):
@@ -58,56 +126,19 @@ def __update_robot__(bot_name):
     If not, the user must have developed the necessary files to handle those dependencies of the robots.
     If neither of them are completed, this will show an error message to the user.
     """
-    sys.path.append('../developing')
-    from node.libs.myjson import MyJson
+    json_services_classes, json_components_classes = __extract_element__(bot_name)
 
     #  github = Github(login_or_token="17143d9e9f8b00012627e2545eefce8fda07d216")
-    #    print(github.get_project(id=0))
-    #   github = Github(base_url='https://github.com/Pyro4Bot-RoboLab/Components',
-    #                   login_or_token='17143d9e9f8b00012627e2545eefce8fda07d216')
-
     github = Github("pyro4bot", "90racano")
 
-    org = github.get_user().get_orgs()[0]
-    print(org)
-    repository = org.get_repo('Components')
-    print(repository)
-    stable = repository.get_contents(path='components/stable')
-    developing = repository.get_contents(path='components/developing')
-    for com in stable:
-        print(com)
-        print(com.path)
-        print(type(com))
+    repository = github.get_user().get_orgs()[0].get_repo('Components')
+    url_services = __find_element__(module='services', json_module_classes=json_services_classes, repository=repository)
+    url_components = __find_element__(module='components', json_module_classes=json_components_classes,
+                                      repository=repository)
+    __download_element__(bot_name, url_services)
+    __download_element__(bot_name, url_components)
 
-    # sha = get_sha_for_tag(repository, 'infrared')
-    # print(sha)
-
-    #   download_directory(repository=repository, server_path='stable/infrared')
-
-    _path_ = os.path.join('robots', bot_name)
-    __json_bot_file__ = os.path.join(_path_, bot_name + '.json')
-    json = MyJson(filename=__json_bot_file__).json
-
-    services_classes = []
-    components_classes = []
-
-    for (key, value) in json['services'].items():
-        class_name = value['cls']
-        services_classes.append(class_name)
-        #    if class_name in [com.path for com in stable]:
-        #       urllib.request.urlretrieve(com.path)
-        file_url = __url__ + 'services/'  # TODO: not completed
-        file = _path_ + 'services/' + class_name + '.py'
-    #    urllib.request.urlretrieve(file_url, file)  # TODO: not completed
-
-    print("services classes")
-    print(services_classes)
-
-    for (key, value) in json['components'].items():
-        components_classes.append(value['cls'])
-
-    print(services_classes)
-    print(components_classes)
+    __download_start__(bot_name)
 
 
 def __create_robot__(bot_name):
@@ -146,6 +177,7 @@ if __name__ == "__main__":
                 argument = sys.argv[2]
                 argument.replace('.json', '')  # just having the precaution of not misspelling the name
                 __update_robot__(argument)
+                print("ROBOT READY TO RUN")
         else:
             argument = sys.argv[1]
             if argument == '-update':
@@ -154,6 +186,7 @@ if __name__ == "__main__":
             else:
                 argument.replace('.json', '')  # just having the precaution of not misspelling the name
                 __create_robot__(argument)
+                print("BASE ROBOT CREATED")
     except (KeyboardInterrupt, SystemExit):
         os._exit(0)
     except Exception:
