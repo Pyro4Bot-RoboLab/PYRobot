7119de1e1d0f547faa59cca4d27a065d04e212ba
Alberto <albertoserranoleon@gmail.com>
2019-02-26 13:23:50 +0100

    Updated start from template

--- developing/generator/template_robot/start.py
+++ developing/generator/template_robot/start.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ____________developed by paco andres____________________
+# _________collaboration with cristian vazquez____________
 """PYRO4BOT Launcher.
 
 Launcher file
 """
 import sys
 import os
-import time
 import json
+import time
 from termcolor import colored
 import setproctitle
-
 
 def get_PYRO4BOT_HOME():
     """ It turns back the environment path of the program Pyro4Bot """
@@ -22,26 +24,26 @@ def get_PYRO4BOT_HOME():
 
 
 PYRO4BOT_HOME = get_PYRO4BOT_HOME()
+print(PYRO4BOT_HOME)
 sys.path.append(PYRO4BOT_HOME)
 
 from node.libs.terminal import Terminal
 from node import robotstarter as robot
 from node.libs import utils
-# from node.libs import inspection
 import components
 import services
-
+NAME_ROBOT = "<robot>"
 if __name__ == "__main__":
     try:
-        jsonbot = os.path.abspath(
-            os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "model", "<robot>" + ".json"))
-        # inspection._classes,inspection._module_errors = inspection.inspecting_modules("services","components")
-        # inspection._classes_lib,inspection._modules_libs_errors = inspection.inspecting_modules("node.libs")
-        PROCESS = robot.starter(filename=jsonbot)
-        setproctitle.setproctitle("PYRO4BOT." + PROCESS[0] + "." + "Console")
-        ROB = utils.get_pyro4proxy(PROCESS[1], PROCESS[0])
+        while True:
+            jsonbot = os.path.abspath(
+                os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), "model", NAME_ROBOT + ".json"))
+            PROCESS = robot.starter(filename=jsonbot,params={})
+            setproctitle.setproctitle("PYRO4BOT." + PROCESS[0] + "." + "Console")
+            #ROB = utils.get_pyro4proxy(PROCESS[1], PROCESS[0])
 
-        Terminal(ROB, PROCESS)
+            Terminal(PROCESS[0],PROCESS[1],pid=PROCESS[3])
+
     except IOError:
         print("The file can not be found: %s" % jsonbot)
     except (KeyboardInterrupt, SystemExit):
