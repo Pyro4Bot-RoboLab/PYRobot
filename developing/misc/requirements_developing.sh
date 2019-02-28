42d7817c8782aee8ec5b726087c7e09554fece49
Alberto <albertoserranoleon@gmail.com>
2019-02-28 17:44:55 +0100

    new dependencies

--- developing/misc/requirements_developing.sh
+++ developing/misc/requirements_developing.sh
@@ -5,8 +5,10 @@ sudo apt install python3-pip
 sudo apt install atom
 # for a raspberry pi Zero, don't install opencv:
 # for the robot, install:
-sudo pip3 install opencv-contrib-python
+pip3 install opencv-contrib-python
 sudo apt install git
-sudo pip3 install green
-sudo pip3 install PyGitHub
+pip3 install keyboard
+pip3 install green
+pip3 install prompt_toolkit
+pip3 install PyGitHub
 pip3 install -r "requirements.txt"
